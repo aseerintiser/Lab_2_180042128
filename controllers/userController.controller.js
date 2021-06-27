c54857ea6f09bb20c774ed54d3f651736aa1e095
Aseer Intiser <aseerniloy@gmail.com>
2021-06-27 15:59:58 +0600

    work on controllers

---
+++ controllers/userController.controller.js
@@ -0,0 +1,5 @@
+const getLoginPage = (req, res) => {
+    res.send("Love RobIbtid from Controller");
+}
+
+module.exports = {getLoginPage};
--- routes/userRoutes.routes.js
+++ routes/userRoutes.routes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const userController = require('../controllers/userController.controller');
 
-router.get('/login', (req, res) => {
-    res.send("Love RobIbtid");
-})
+router.get('/login', userController.getLoginPage);
 
 module.exports = router;

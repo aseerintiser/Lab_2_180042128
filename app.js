b1066efb29488673ed9731909312606d67785a4a
Aseer Intiser <aseerniloy@gmail.com>
2021-06-27 15:51:51 +0600

    work on routing

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const cors = require("cors");
+const userRoutes = require('./routes/userRoutes.routes')
+
 const app = express();
+app.use(cors());
+
+app.use(userRoutes);
+
+app.get("/", (req, res) => {
+    res.sendFile("index3.html", {root:"./views"});
+  });
+
 
 module.exports = app;
